92286cf50eabe9e0294c0f9612a8673a22b22b1f
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-07-27 23:06:22 +0200

    Changed guest start function

--- guest-as-application/guest/src/lib.rs
+++ guest-as-application/guest/src/lib.rs
@@ -1,31 +1,21 @@
 #![no_main]
 
-use wasip1_i2c::guest::I2cResource;
+use wasip1_i2c::{ common::I2cAddress, guest::I2cResource };
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() {
+    let data = vec![0x12, 0xac, 0xce];
+    let slave_addr: I2cAddress = 0x5678;
     let device = I2cResource::new();
-    let device2 = I2cResource::new();
-    let res = device.read(0xabcd, 3);
-    if let Ok(data) = res {
-        println!("Guest: Read data: {:?}", data);
-    }
-    // let _ = device2.read(0x1234, 3);
-    let _ = device.write(0x5678, &vec![0x12, 0xac, 0xce]);
-    /* match device.read(0xabcd, 3) {
-        Ok(res) => 100,
-        Err(err_code) =>
-            match err_code {
-                ErrorCode::Bus => 0b000_00000,
-                ErrorCode::ArbitrationLoss => 0b001_00000,
-                ErrorCode::NoAcknowledge(no_acknowledge_source) =>
-                    match no_acknowledge_source {
-                        i2c::NoAcknowledgeSource::Address => 0b010_00000,
-                        i2c::NoAcknowledgeSource::Data => 0b010_00001,
-                        i2c::NoAcknowledgeSource::Unknown => 0b010_00010,
-                    }
-                ErrorCode::Overrun => 0b011_00000,
-                ErrorCode::Other => 0b100_00000,
+    match device.write(slave_addr, &data) {
+        Ok(()) => {
+            match device.read(slave_addr, data.len()) {
+                Ok(read_data) => println!("Guest: Read data: {:?}", read_data),
+                Err(code) => eprintln!("Guest: Read: {:?}", code),
             }
-    }; */
+        }
+        Err(code) => {
+            eprintln!("Guest: Write: {:?}", code);
+        }
+    }
 }
