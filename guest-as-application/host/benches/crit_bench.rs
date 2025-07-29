e458424330487c8329e04b64e03078812bf52570
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-07-29 04:58:29 +0200

    flamegraph tryout

--- guest-as-application/host/benches/crit_bench.rs
+++ guest-as-application/host/benches/crit_bench.rs
@@ -1,5 +1,6 @@
 use std::hint::black_box;
 use criterion::{ criterion_group, criterion_main, Criterion };
+use pprof::criterion::{ PProfProfiler, Output };
 use host::wamr_manager;
 
 fn full_run() {
@@ -13,8 +14,18 @@ fn full_run() {
 
 fn criterion_benchmark(c: &mut Criterion) {
     let fntest = black_box(full_run);
-    c.bench_function("I2C Ping Pong", |b| b.iter(|| fntest()));
+    c.bench_function("I2C Ping Pong", |b|
+        b.iter(|| {
+            let guard = PProfProfiler::new(50, Output::Flamegraph(None));
+            black_box(full_run());
+        })
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
+/* criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler();
+    targets = criterion_benchmark,
+} */
 criterion_main!(benches);
