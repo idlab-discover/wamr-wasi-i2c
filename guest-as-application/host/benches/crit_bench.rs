455b083ab6e35d2b2cbc3ee5f97530edca93117b
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-07-29 03:35:36 +0200

    criterion ping pong

--- guest-as-application/host/benches/crit_bench.rs
+++ guest-as-application/host/benches/crit_bench.rs
@@ -2,9 +2,18 @@ use std::hint::black_box;
 use criterion::{ criterion_group, criterion_main, Criterion };
 use host::wamr_manager;
 
+fn full_run() {
+    let runtime = wamr_manager::setup_runtime().expect("Bench: Full: Runtime Failed");
+    let module = wamr_manager::setup_module(&runtime).expect("Bench: Full: Module Failed");
+    let instance = wamr_manager
+        ::setup_module_instance(&runtime, &module)
+        .expect("Bench: Full: Instance Failed");
+    wamr_manager::run_guest_function(&instance).expect("Bench: Full: Function Failed");
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
-    let fntest = black_box(wamr_manager::setup_runtime);
-    c.bench_function("fib 20", |b| b.iter(|| fntest()));
+    let fntest = black_box(full_run);
+    c.bench_function("I2C Ping Pong", |b| b.iter(|| fntest()));
 }
 
 criterion_group!(benches, criterion_benchmark);
