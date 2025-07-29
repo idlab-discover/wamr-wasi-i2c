f516f754a323ac62a0f2993bdd29154e46cbd97f
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-07-29 05:28:48 +0200

    Switch to just function of module analysis

--- guest-as-application/host/benches/crit_bench.rs
+++ guest-as-application/host/benches/crit_bench.rs
@@ -2,7 +2,27 @@ use std::hint::black_box;
 use criterion::{ criterion_group, criterion_main, Criterion };
 use host::wamr_manager;
 
-fn full_run() {
+use wamr_rust_sdk::{ instance::Instance, module::Module, runtime::Runtime };
+fn setup_once() -> (Runtime, Module, Instance) {
+    // Doe setup eenmalig buiten de benchmark
+    let runtime = wamr_manager::setup_runtime().unwrap();
+    let module = wamr_manager::setup_module(&runtime).unwrap();
+    let instance = wamr_manager::setup_module_instance(&runtime, &module).unwrap();
+    (runtime, module, instance)
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let (_runtime, _module, instance) = setup_once();
+
+    c.bench_function("I2C Ping Pong", |b| {
+        b.iter(|| {
+            // Meet alleen de functie-aanroep
+            black_box(wamr_manager::run_guest_function(&instance).unwrap())
+        })
+    });
+}
+
+/* fn full_run() {
     let runtime = wamr_manager::setup_runtime().expect("Bench: Full: Runtime Failed");
     let module = wamr_manager::setup_module(&runtime).expect("Bench: Full: Module Failed");
     let instance = wamr_manager
@@ -14,7 +34,7 @@ fn full_run() {
 fn criterion_benchmark(c: &mut Criterion) {
     let func = black_box(full_run);
     c.bench_function("I2C Ping Pong", |b| b.iter(func));
-}
+} */
 
 criterion_group!(benches, criterion_benchmark);
 /* criterion_group! {
