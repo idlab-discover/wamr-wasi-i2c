c8cfffdcd9f0fa685661ef1de331bc4b98e11177
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-07-31 03:10:59 +0200

    Added another wamr test

--- guest-as-application/benchall/src/dhat_tests.rs
+++ guest-as-application/benchall/src/dhat_tests.rs
@@ -1,23 +1,30 @@
 fn wamr_test() {
-    let _profiler = dhat::Profiler::builder().file_name("wamr_impl.json").build();
+    let _profiler = dhat::Profiler::builder().file_name("wamr_test1.json").build();
     let runtime = wamr_impl::setup_runtime().expect("Oeps");
     let module = wamr_impl::setup_module(&runtime).expect("Oeps");
-    {
-        let instance = wamr_impl::setup_module_instance(&runtime, &module).expect("Oeps");
-        wamr_impl::run_guest_function(&instance).expect("Oeps");
-    }
+    let instance = wamr_impl::setup_module_instance(&runtime, &module).expect("Oeps");
+    wamr_impl::run_guest_function(&instance).expect("Oeps");
+}
+
+fn wamr_test2() {
+    let runtime = wamr_impl::setup_runtime().expect("Oeps");
+    let module = wamr_impl::setup_module(&runtime).expect("Oeps");
+
+    let instance = wamr_impl::setup_module_instance(&runtime, &module).expect("Oeps");
+    let _profiler = dhat::Profiler::builder().file_name("wamr_test2.json").build();
+    wamr_impl::run_guest_function(&instance).expect("Oeps");
 }
 
 fn native_test() {
-    let _profiler = dhat::Profiler::builder().file_name("native_impl1.json").build();
+    let _profiler = dhat::Profiler::builder().file_name("native_test1.json").build();
     let mut hw = native_impl::setup();
     native_impl::pingpong(&mut hw);
 }
 
 fn native_test2() {
     let mut hw = native_impl::setup();
-    let _profiler = dhat::Profiler::builder().file_name("native_impl2.json").build();
+    let _profiler = dhat::Profiler::builder().file_name("native_test2.json").build();
     native_impl::pingpong(&mut hw);
 }
 
-pub static TESTS: [fn(); 3] = [wamr_test, native_test, native_test2];
+pub static TESTS: [fn(); 4] = [wamr_test, wamr_test2, native_test, native_test2];
