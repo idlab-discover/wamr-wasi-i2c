14e60a75fab6f59dea447d2158fb077c22bd3675
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-08-02 05:10:58 +0200

    Removed wasi import needs in guest p2 so they can be removed from host as well

--- guest-as-application/wasip2-i2c-guest/src/lib.rs
+++ guest-as-application/wasip2-i2c-guest/src/lib.rs
@@ -11,8 +11,8 @@ impl Guest for PingPongComponent {
     /// Say hello!
     fn run() {
         let dev = get_i2c_bus(1);
-        dev.write(0x09, &[0x68, 0x65, 0x6c, 0x6c, 0x6f]).expect("Guest: Write error");
-        let rr = dev.read(0x09, 5).expect("Guest: Failed reading data");
+        let _ = dev.write(0x09, &[0x68, 0x65, 0x6c, 0x6c, 0x6f]);
+        let _ = dev.read(0x09, 5);
     }
 }
 
