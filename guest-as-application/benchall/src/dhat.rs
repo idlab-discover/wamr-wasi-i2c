d1b68ceca17a4fbe8a708264b7e82aef3c73339a
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-08-04 00:29:06 +0200

    Added comments about weird wamr behaviour

--- guest-as-application/benchall/src/dhat.rs
+++ guest-as-application/benchall/src/dhat.rs
@@ -40,6 +40,9 @@ fn native_pingpong() {
 #[cfg(feature = "dhat-runtime")]
 fn wamr_setup() {
     let _profiler = dhat::Profiler::builder().file_name("wamr_setup.json").build();
+    // TODO: Bespreek: WAMR doet iets heel vreemd: Claude (Rust Conditional Feature Compilation)
+    //      Strace geeft weer dat WAMR nog vanalles aan het opzetten zou zijn wanneer we de setup zouden aanroepen via let _ = ...
+    //      Dit zou zijn doordat de destructor meteen wordt opgeroepen
     let (_rt, _mod, _instance, _f) = wamr_impl
         ::setup_runtime()
         .expect("[BENCH:dhat] WAMR runtime setup failed");
