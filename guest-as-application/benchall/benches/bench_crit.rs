3b1ba2b886491563ee88cdebb546342538763191
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-08-03 05:54:25 +0200

    Proper benchmark module handling

--- guest-as-application/benchall/benches/bench_crit.rs
+++ guest-as-application/benchall/benches/bench_crit.rs
@@ -1,26 +1,11 @@
-mod crit_tests;
+mod modules;
+use modules::crit_tests::criterion_benchmark;
+use criterion::{ criterion_group, criterion_main, Criterion };
+use std::time::Duration;
 
-use crit_tests::criterion_benchmark;
-use criterion::{ criterion_group, criterion_main };
-
-/* fn full_run() {
-    let runtime = wamr_manager::setup_runtime().expect("Bench: Full: Runtime Failed");
-    let module = wamr_manager::setup_module(&runtime).expect("Bench: Full: Module Failed");
-    let instance = wamr_manager
-        ::setup_module_instance(&runtime, &module)
-        .expect("Bench: Full: Instance Failed");
-    wamr_manager::run_guest_function(&instance).expect("Bench: Full: Function Failed");
-}
-
-fn criterion_benchmark(c: &mut Criterion) {
-    let func = black_box(full_run);
-    c.bench_function("I2C Ping Pong", |b| b.iter(func));
-} */
-
-criterion_group!(benches, criterion_benchmark);
-/* criterion_group! {
+criterion_group! {
     name = benches;
-    config = Criterion::default().with_profiler();
+    config = Criterion::default().measurement_time(Duration::from_secs(10));
     targets = criterion_benchmark,
-} */
+}
 criterion_main!(benches);
---
+++ guest-as-application/benchall/benches/modules/mod.rs
@@ -0,0 +1,1 @@
+pub mod crit_tests;
