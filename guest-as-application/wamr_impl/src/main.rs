c967c07198ad54751ae58d2edf9f5a2b3e294d79
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-08-03 20:58:12 +0200

    Fixed wamr bin

--- guest-as-application/wamr_impl/src/main.rs
+++ guest-as-application/wamr_impl/src/main.rs
@@ -1,6 +1,6 @@
 use wamr_impl::{ setup_runtime, run_pingpong };
 
 fn main() {
-    let (_runtime, _module, instance) = setup_runtime().expect("Oeps");
-    run_pingpong(&instance).expect("Oeps");
+    let (_runtime, _module, instance, func) = setup_runtime().expect("[WAMR] Runtime Setup Failed");
+    run_pingpong(&instance, &func).expect("[WAMR] Running PingPong Failed");
 }
