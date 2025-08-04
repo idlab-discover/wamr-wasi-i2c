c5267628749a5ae06f24b75d883290a68ecdf0d3
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-08-04 03:02:35 +0200

    pprof tryouts 4

--- guest-as-application/benchall/src/pprof.rs
+++ guest-as-application/benchall/src/pprof.rs
@@ -3,12 +3,7 @@ use std::fs::File;
 use pprof::ProfilerGuard;
 
 fn get_guard() -> ProfilerGuard<'static> {
-    pprof::ProfilerGuardBuilder
-        ::default()
-        .frequency(1000)
-        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
-        .build()
-        .unwrap()
+    pprof::ProfilerGuardBuilder::default().frequency(1000).build().unwrap()
 }
 
 fn wamr_pingpong() {
@@ -16,16 +11,16 @@ fn wamr_pingpong() {
         ::setup_runtime()
         .expect("[BENCH:pprof] WAMR runtime setup failed");
 
-    /* let _guard = get_guard(); */
+    let _guard = get_guard();
 
     wamr_impl::run_pingpong(&instance, &f).expect("[BENCH:pprof] WAMR pingpong failed");
 
-    /* if let Ok(report) = _guard.report().build() {
+    if let Ok(report) = _guard.report().build() {
         let file = File::create("wamr_flame.svg").unwrap();
         let mut options = pprof::flamegraph::Options::default();
         options.image_width = Some(2500);
         report.flamegraph_with_options(file, &mut options).unwrap();
-    } */
+    }
 }
 
 fn wasmtime_pingpong() {
@@ -33,35 +28,33 @@ fn wasmtime_pingpong() {
         ::setup_runtime()
         .expect("[BENCH:crit] Wasmtime runtime setup failed");
 
-    // let _guard = std::hint::black_box(get_guard());
+    let _guard = std::hint::black_box(get_guard());
 
     wasmtime_impl
         ::run_pingpong(&instance, &mut store)
         .expect("[BENCH:pprof] Wasmtime pingpong failed");
 
-    /* if let Ok(report) = _guard.report().build() {
+    if let Ok(report) = _guard.report().build() {
         let file = File::create("wasmtime_flame.svg").unwrap();
         let mut options = pprof::flamegraph::Options::default();
         options.image_width = Some(2500);
         report.flamegraph_with_options(file, &mut options).unwrap();
-    } */
+    }
 }
 
 fn native_pingpong() {
     let mut hw = native_impl::setup();
 
-    // let _guard = std::hint::black_box(get_guard());
+    let _guard = std::hint::black_box(get_guard());
 
     native_impl::pingpong(&mut hw);
 
-    /* if let Ok(report) = _guard.report().build() {
-        println!("Generating native_flame: {:?}", report);
-
+    if let Ok(report) = _guard.report().build() {
         let file = File::create("native_flame.svg").unwrap();
         let mut options = pprof::flamegraph::Options::default();
         options.image_width = Some(2500);
         report.flamegraph_with_options(file, &mut options).unwrap();
-    } */
+    }
 }
 
 #[cfg(feature = "pprof-runtime")]
@@ -97,19 +90,9 @@ fn main() {
         wasmtime_setup();
     });
 
-    let guard = ProfilerGuard::new(1000).unwrap();
     std::hint::black_box({
-        println!("Starting pprof runs");
         native_pingpong();
         wamr_pingpong();
         wasmtime_pingpong();
     });
-    if let Ok(report) = guard.report().build() {
-        println!("Generating native_flame: {:?}", report);
-
-        let file = File::create("all_flame.svg").unwrap();
-        let mut options = pprof::flamegraph::Options::default();
-        options.image_width = Some(2500);
-        report.flamegraph_with_options(file, &mut options).unwrap();
-    }
 }
