8d5b512b91de37c2d4e9724e668b6b274bd4d3e2
Robin Verbeelen <robin.verbeelen@gmail.com>
2025-08-05 21:05:53 +0200

    wasmtime auto sampling

--- guest-as-application/benchall/benches/bench_crit.rs
+++ guest-as-application/benchall/benches/bench_crit.rs
@@ -9,7 +9,7 @@ use std::time::Duration;
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().measurement_time(Duration::from_secs(30)).warm_up_time(Duration::from_secs(8));
+    config = Criterion::default().measurement_time(Duration::from_secs(110)).warm_up_time(Duration::from_secs(8));
     targets = bench_setup_comparison, bench_cold_pingpong_comparison, bench_hot_pingpong_comparison
 }
 criterion_main!(benches);
